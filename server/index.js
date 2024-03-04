da6b5a9cec4c3c7956db3ae581570dcf9914c858
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2024-03-04 13:41:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,41 @@
-const express = require('express');
+//Importing all the dependencies
 
-// Created an Express application
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-// Defined a route
-app.get('/', (req, res) => {
-  res.send('Hey, there my server is live! 😁😁😁😁😁😁');
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Connecting to MongoDB 
+const { MongoClient } = require('mongodb');
+const port = 3000
+app.use(cors())
+
+const urii = 'mongodb+srv://dev326patil:devrajcapstone326@capstone.riouq2l.mongodb.net/?retryWrites=true&w=majority&appName=Capstone';
+const client = new MongoClient(urii);
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB Atlas Successfully');
+    const database = client.db('Whole_data');
+    const collection = database.collection('Overall');
+    
+    // Creating a get request 
+
+    
+    app.get('/', async (req,res)=>{
+    const result = await collection.find({}).toArray(
+        
+    );
+      res.json(result);
+    })
+  })
+  .catch(err => {
+    console.error('An Error while connecting to MongoDB Atlas', err);
 });
 
-// Started the server
-const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
