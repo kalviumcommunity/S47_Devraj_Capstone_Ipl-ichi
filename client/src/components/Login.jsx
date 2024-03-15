0d49ca3254902a4fda5e25a155c49d9a5c64a484
Devraj326 <dev326patil@gmail.com>
2024-03-15 14:44:41 +0530

    backend done  for login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios'; // Import Axios
-// import './Login.css';
+import './Login.css';
 
 const Login = () => {
   const [isActive, setIsActive] = useState(false); // State to track the active class
---
+++ server/controllers/usercontroller.js
@@ -0,0 +1,13 @@
+// controllers/usersController.js
+const Users = require('../models/users/users');
+
+const registerUsersController = async (req, res) => {
+    try {
+        // Use the create method to insert a new tutor document
+        const users = await Users.create(req.body);
+        res.status(201).send(users);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+module.exports = registerUsersController
