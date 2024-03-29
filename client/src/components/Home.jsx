f2fdd5471c6fa7e46fe8f8d434f38cfd003089b8
Devraj326 <dev326patil@gmail.com>
2024-03-29 16:17:33 +0530

    added news with using of api and also created sorting function for the calender

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,57 +1,103 @@
-// Home Page
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styles from './Home.module.css';
+import TerminalLoader from './TerminalLoader';
 
 const Home = () => {
   const [matches, setMatches] = useState([]);
+  const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
+  const [visibleMatches, setVisibleMatches] = useState(5);
+  const [date, setDate] = useState(new Date());
+  const [dateSelected, setDateSelected] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/');
-        setMatches(response.data[0].matches);
+        const matchResponse = await axios.get('http://localhost:3000/');
+        setMatches(matchResponse.data[0].matches);
+
+        const newsResponse = await axios.get('https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=1c1fd78b22794f30878e63a99a5a113b');
+        setNews(newsResponse.data.articles);
       } catch (error) {
         console.error('Error fetching data: ', error);
         setError('Error fetching data. Please try again later.');
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
+  const toggleMatchesDisplay = () => {
+    setVisibleMatches(visibleMatches === 5 ? (dateSelected ? matches.filter(match => new Date(match.date).toDateString() === date.toDateString()).length : matches.length) : 5);
+  };
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+    setDateSelected(true);
+    setVisibleMatches(5);
+  };
+
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
-    <div className={styles.main}>
-      <div className={styles.listmatch}>
-        {matches.map((match, index) => (
-          <div className={styles.team} key={index}>
-            
+    <div>
+      <TerminalLoader isLoading={loading} fullScreen={loading} />
+
+      <div className={styles.main}>
+        <div className={styles.listmatch}>
+          {matches
+            .filter((match) => {
+              return !dateSelected || new Date(match.date).toDateString() === date.toDateString();
+            })
+            .slice(0, visibleMatches)
+            .map((match, index) => (
+              <div className={styles.team} key={index}>
                 <article className={styles.card__article}>
-                  <img src={match.matchimg} alt="image" className={styles.card__img}/>
+                  <img src={match.matchimg} alt="Match" className={styles.card__img}/>
                   <div className={styles.card__data}>
                     <span className={styles.card__description}>{match.stadium}</span>
                     <div className={styles.flexteam}>
-                    <h2 className={styles.card__title}>{match.team1_short}</h2> <p>vs</p> <h2 className={styles.card__title}>{match.team2_short}</h2>
+                      <h2 className={styles.card__title}>{match.team1_short}</h2> <p>vs</p> <h2 className={styles.card__title}>{match.team2_short}</h2>
                     </div>
-                    <a href="#" className={styles.card__button}>See Team</a>
+                    {/* Use Link to navigate to Player component with team short names as route params */}
+                    <Link to={{ pathname: `/player`, state: { match1: match.team1_short } }} className={styles.card__button}>See Team</Link>
+
+
                   </div>
                 </article>
-              
-            
+              </div>
+            ))}
+          <button onClick={toggleMatchesDisplay} className={styles.toggleButton} role="button">
+            {visibleMatches > 5 || (!dateSelected && visibleMatches >= matches.length) ? 'Show Less' : 'See All'}
+          </button>
+        </div>
+
+        <div className={`${styles.calendar} ${styles.calendarWrapper} ${styles.calendarCustomPosition}`}>
+          <Calendar onChange={handleDateChange} value={date} />
+        </div>
+
+      </div>
+
+      <div className={styles.matchhighlights}>
+        {news.slice(0, 5).map((article, index) => (
+          <div key={index} className={`${styles.newsItem} ${index % 2 === 0 ? styles.largeNewsItem : styles.smallNewsItem}`}>
+            <h3>{article.title}</h3>
+            <div className={styles.newsContent}>
+              <p>{article.description}</p>
+              <img src={article.urlToImage} alt="News" className={styles.newsImage}/>
+            </div>
           </div>
         ))}
       </div>
-      <div className={`${styles.calendar} ${styles.calendarWrapper} ${styles.calendarCustomPosition}`}>
-          <Calendar onChange={handleDateChange} value={date} />
-        </div>
-      <div className={styles.matchhighlights}></div>
     </div>
   );
 };
